78e9b4d173ef30b7ac7cf44fb35e9c2667a52cd7
Eytan Benittah <eytanbenittah@gmail.com>
2024-11-07 16:12:13 +0200

    Codes

---
+++ Complexity/Class_15/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        int num1;
+        System.Console.WriteLine("This is a test!");
+        num1 = int.Parse(Console.ReadLine());
+        Console.WriteLine(num1);
+    }
+}
