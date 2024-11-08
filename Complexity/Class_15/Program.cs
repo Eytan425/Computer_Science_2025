34dab0141bda36fe945cd26240f571624c69242e
Eytan Benittah <eytanbenittah@gmail.com>
2024-11-08 15:46:42 +0200

    Removed the code that wasn't working and added an introduction

--- Complexity/Class_15/Program.cs
+++ Complexity/Class_15/Program.cs
@@ -5,6 +5,6 @@ class Program
 {
     public static void Main()
     {
-        Console.WriteLine("We are making our own linkedlist class called Node and will use that in class");
+        Console.WriteLine("We are making our own linkedlist class called Node and will use that in class\n");
     }   
 }
